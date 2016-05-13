947f14815939c763d92058f6dbc754169af81ee8
brunoCerberus <bruno.guitarpro@gmail.com>
2016-05-13 12:21:21 -0300

    about_screen updated MainActivity updated information_screen updated list_item added AndroidManifest errors fixed

--- app/src/main/java/com/example/bruno/frontend/MainActivity.java
+++ app/src/main/java/com/example/bruno/frontend/MainActivity.java
@@ -4,6 +4,13 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import java.lang.String;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,21 +25,34 @@ public class MainActivity extends AppCompatActivity {
 
     // authentication method
     public void login(View view){
+
         setContentView(R.layout.function_screen);
     }
 
     // set the information_screen to the screen
     public void information(View view){
+
         setContentView(R.layout.information_screen);
+
+        RadioButton male_radio = (RadioButton) findViewById(R.id.sex_male_radio);
+        RadioButton female_radio = (RadioButton) findViewById(R.id.sex_female_radio);
+
+        if (male_radio.isActivated())
+            female_radio.setActivated(false);
+
+        if (female_radio.isActivated())
+            male_radio.setActivated(false);
     }
 
     //set the request_screen to the screen
     public void request(View view) {
+
         setContentView(R.layout.request_screen);
     }
 
     // set the offer_screen to the screen
     public void host(View view) {
+
         setContentView(R.layout.offer_screen);
     }
 
@@ -50,6 +70,14 @@ public class MainActivity extends AppCompatActivity {
     //set about_screen to the screen
     public void about(View view) {
         setContentView(R.layout.about_screen);
+
+        ListView list = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> adapter;
+        ArrayList<String> myString;
+        String[] items = {"Bruno Lopes", "bacon", "banana", "orange", "tomator", "ghost"};
+        myString = new ArrayList<>(Arrays.asList(items));
+        adapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.txtitem, myString);
+        list.setAdapter(adapter);
 
     }
 
